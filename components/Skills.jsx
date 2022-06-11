71a7624a2ea8f7d99d12bf727d4831bde2f20d5d
Gokyan <52583430+bimbaquingoch@users.noreply.github.com>
2022-06-11 12:29:37 -0500

    prueba variables vercel

---
+++ components/Skills.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+
+const Skills = ({ habilidades }) => {
+  return habilidades.map((habilidad) => (
+    <div key={habilidad._id}>
+      <div className='rounded-full border-2 border-red-300 cursor-pointer overflow-hidden h-14 w-14'>
+        <img
+          className='w-full'
+          src={`${habilidad.im_skill.url}`}
+          alt={`${habilidad.skill}`}
+        />
+      </div>
+      <p className='font-light text-sm truncate text-slate-600 w-14 mt-1 text-center'>
+        {habilidad.skill}
+      </p>
+    </div>
+  ));
+};
+
+export default Skills;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Contenido from "../components/Contenido";
 import Header from "../components/Header";
 
-const Home = () => {
+const Home = ({ habilidades }) => {
   return (
     <div className='h-screen w-full bg-slate-300 overflow-x-hidden'>
       <Head>
@@ -12,9 +12,17 @@ const Home = () => {
       </Head>
       <Header />
 
-      <Contenido />
+      <Contenido habilidades={habilidades} />
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const url = `${process.env.API_URL}/my-skills`;
+  const respuesta = await fetch(url);
+  const habilidades = await respuesta.json();
+
+  return { props: { habilidades } };
 };
 
 export default Home;
