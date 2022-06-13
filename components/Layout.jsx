90af173e6a8d8a767cf4513d0609521931189291
Gokyan <52583430+bimbaquingoch@users.noreply.github.com>
2022-06-13 16:26:36 -0500

    meta tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,8 +9,37 @@ const Layout = ({ children, page }) => {
     <div className='layout'>
       <Head>
         <title>Bimb - {page}</title>
-        <meta name='description' content='Portafolio creado con Nextjs' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name='title' content={`Bimb - ${page}`} />
+        <meta
+          name='description'
+          content='Nuevo portafolio creado con NEXTJS y strapi'
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:url'
+          content='https://next-portafolio-psi.vercel.app/'
+        />
+        <meta property='og:title' content='Bimb - home' />
+        <meta
+          property='og:description'
+          content='Portafolio creado con Nextjs'
+        />
+        <meta property='og:image' content='' />
+
+        {/* <!-- Twitter --> */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta
+          property='twitter:url'
+          content='https://next-portafolio-psi.vercel.app/'
+        />
+        <meta property='twitter:title' content='Bimb - home' />
+        <meta
+          property='twitter:description'
+          content='Portafolio creado con Nextjs'
+        />
+        <meta property='twitter:image' content='' />
       </Head>
       <Header />
       {theme === "dark" && <Burbujas />}
